0494e7b49da03056b7949348aea4036a21efe1ad
Bryce Stradling <brycestradling@gmail.com>
2018-09-20 01:45:49 -0400

    added more content to views

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
-main#Contact-content
-  .title
-    h1 Contact
-  form#contactForm
-    h2.c-header DEAR BRYCE
-    fieldset
-    label My Name is
-    input(type="text" placeholder="Name")
-    fieldset
-    label you can reach me at
-    input(type="email" placeholder="Email")
-    textarea(placeholder="myWord(I do Declare);")
-    input(type="submit" value="send")
+  main#Contact
+    #Contact-content
+      .title
+        h1 Contact
+      form#contactForm
+        h2.c-header DEAR BRYCE
+        fieldset
+          label My Name is
+          input(type="text" placeholder="(Name)")
+        fieldset
+          label you can reach me at
+          input(type="email" placeholder="(Email)")
+        textarea(name="message", cols="30", rows="10" placeholder="myWord(I do Declare);")
+        input(type="submit" value="send")
 
 </template>
 <script>
@@ -22,56 +23,81 @@ main#Contact-content
 <style lang="sass" scoped>
 @import '../sass/vars'
 
+// CONTACT Page
+
+#Contact
+  background: url(../assets/contact-bg.svg)
+  background-size: cover
+  height: 100vh
+  display: flex
+  align-content: center
+  justify-content: center
 #Contact-content
-    // background: url(../../dist/img/contact-bg.svg)
-    display: flex
-    align-content: center
-    justify-content: center
+  align-self: center
+  display: grid
+  grid-template-columns: repeat(5, 100px)
+  grid-template-rows: repeat(6, 100px)
+  .title
+    grid-row: 1 / span 1
+    grid-column: 1 / span 4
+    z-index: 1
+    text-align: center
+    h1
+      font-weight: 900
+
 #contactForm
-    background: $black
-    padding: 1em
-    color: white
-    max-width: 30%
-    align-self: center
-h2
+  grid-row: 2 / span 6
+  grid-column: 2 / span 5
+  background: $black
+  padding: 1em
+  color: white
+  h2
+    font-weight: 300
     font-size: 3.5em
+    margin-bottom: 0
     text-transform: uppercase
-fieldset
+  fieldset
     border: 0
-    padding: 0
+    padding: 1em 0
     margin: 0
-input,label
+  label
+    margin-right: 1em
+  input,label
     display: inline
     width: 50%
-    //padding: 1em
-textarea
+  textarea
     border: 3px dashed white
     display: block
     width: 100%
-    margin: 1em 0
+    margin: 0.5em 0
+    padding: 1em
     background: $black
     color: white
     &:focus
-    outline: none
-    &::placeholder
+      outline: none
+      &::placeholder
         opacity: 0
         transition: 0.1s ease-out
     &::placeholder
-    color: white
-input
-    padding-left: 0
+      //color: white
+      font-family: monospace
+  input
+    padding: 0.5em
     border: 0
     border-bottom: 3px dashed white
     background: $black
     color: white
     &:focus
-    outline: none
-    &::placeholder
+      outline: none
+      &::placeholder
         opacity: 0
         transition: 0.1s ease-out
     &::placeholder
-    color: white
-input[type="submit"]
+      //color: white
+      font-family: monospace
+      text-align: center
+  input[type="submit"]
+    margin-top: 1em
     display: block
     background: $magenta
     border: 0
@@ -79,4 +105,5 @@ input[type="submit"]
     text-transform: uppercase
     font-size: 1.5em
     width: 100%
+
 </style>
